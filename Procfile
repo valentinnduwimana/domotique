9dba5bfb1218e0dcb012b1affac396a288b4c732
valentinnduwimana <valentinnduwimana5@gmail.com>
2026-06-06 18:25:28 +0200

    Préparation pour Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gestion_lampe_web.wsgi
--- gestion_lampe_web/settings.py
+++ gestion_lampe_web/settings.py
@@ -65,7 +65,20 @@ else:
         }
     }
 
-AUTH_PASSWORD_VALIDATORS = [...]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Europe/Paris'
